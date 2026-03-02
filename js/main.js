a2c33dd33dd6de4a2c21bd2b6cc65fb4be560f30
ole4kakoroleva666 <zezulaolga9@gmail.com>
2026-03-02 17:51:09 +0700

    without7practical

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ let app = new Vue({
    el: '#app',
    data: {
        product: "Socks",
+       brand: 'Vue Mastery',
        description: "A pair of warm, fuzzy socks",
-       image: "./assets/vmSocks-blue-onWhite.jpg",
+       selectedVariant: 0,
        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords",
-       inStock: true,
        inventory: 100,
        onSale: true,
        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
@@ -14,15 +14,34 @@ let app = new Vue({
             variantId: 2234,
             variantColor: 'green',
             variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            variantQuantity: 10
         },
         {
             variantId: 2235,
             variantColor: 'blue',
             variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            variantQuantity: 0
         }
-       ],
+        ],
+
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0
+    },
+
+    computed: {
+
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity > 0
+        },
+
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+
     },
 
     methods:{
@@ -36,8 +55,9 @@ let app = new Vue({
             }
         },          
 
-        updateProduct (variantImage) {
-            this.image = variantImage
+        updateProduct (index) {
+            this.selectedVariant = index
+            console.log(this.variants[index].variantQuantity > 0)   
         }
     }
        
