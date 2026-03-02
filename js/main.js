40880fc8508574873b3853eb6a1943d477ac8542
ole4kakoroleva666 <zezulaolga9@gmail.com>
2026-03-02 15:57:03 +0700

    5-6tasks

---
+++ js/main.js
@@ -0,0 +1,43 @@
+let app = new Vue({
+   el: '#app',
+   data: {
+       product: "Socks",
+       description: "A pair of warm, fuzzy socks",
+       image: "./assets/vmSocks-blue-onWhite.jpg",
+       link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords",
+       inStock: true,
+       inventory: 100,
+       onSale: true,
+       details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+       variants: [
+        {
+            variantId: 2234,
+            variantColor: 'green',
+            variantImage: "./assets/vmSocks-green-onWhite.jpg",
+        },
+        {
+            variantId: 2235,
+            variantColor: 'blue',
+            variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+        }
+       ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0
+    },
+
+    methods:{
+        addToCart() {
+            this.cart += 1
+        },
+
+        deleteToCart() {
+            this.cart -= 1
+        },
+
+        updateProduct (variantImage) {
+            this.image = variantImage
+        }
+    }
+       
+})
+
